8fa89208c8af761b2dc4773ead7b5f27cb9d3eb3
choosenname <vlad@luckashonok.ru>
2024-04-05 16:38:34 +0300

    feat: implement login form

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,13 @@
+import { CardWrapper } from "@/components/auth/card-wrapper"
+
+export const LoginForm = () => {
+    return (
+        <CardWrapper
+            headerLabel="Welcome back"
+            backButtonLabel="Don't have an account?"
+            backButtonHref="/register"
+        >
+            Login Form!
+        </CardWrapper>
+    );
+};
