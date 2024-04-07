dbda88a2e4e47f71bc762c9dd3b91d36c13e498c
choosenname <vlad@luckashonok.ru>
2024-04-07 13:29:16 +0300

    feat: implement register form

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,49 +1,57 @@
 "use client";
 
-import { CardWrapper } from "@/components/auth/card-wrapper"
-import {useForm} from "react-hook-form";
-import {LoginSchema} from "@/schemas";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {z} from "zod";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {FormError} from "@/components/form-error";
-import {FormSuccess} from "@/components/form-success";
-import {login} from "@/actions/login";
-import {useState, useTransition} from "react";
+import * as z from "zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export const LoginForm = () => {
+import { RegisterSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { CardWrapper } from "@/components/auth/card-wrapper"
+import { Button } from "@/components/ui/button";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { register } from "@/actions/register";
+
+export const RegisterForm = () => {
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
     const [isPending, startTransition] = useTransition();
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
         defaultValues: {
             name: "",
             password: "",
         },
     });
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
         setError("");
         setSuccess("");
 
         startTransition(() => {
-            login(values)
+            register(values)
                 .then((data) => {
                     setError(data.error);
                     setSuccess(data.success);
                 });
         });
-    }
+    };
 
     return (
         <CardWrapper
-            headerLabel="Welcome back"
-            backButtonLabel="Don't have an account?"
-            backButtonHref="/register"
+            headerLabel="Create an account"
+            backButtonLabel="Already have an account?"
+            backButtonHref="/login"
         >
             <Form {...form}>
                 <form
@@ -94,7 +102,7 @@ export const LoginForm = () => {
                         type="submit"
                         className="w-full"
                     >
-                        Login
+                        Create an account
                     </Button>
                 </form>
             </Form>
