411806fb97c55b108ed6729c14de5b4511c18cd8
choosenname <vlad@luckashonok.ru>
2024-04-09 20:19:28 +0300

    feat: add modal view

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import {useRouter} from "next/navigation";
+import {Dialog, DialogContent, DialogTrigger} from "@/components/ui/dialog";
+import {LoginForm} from "@/components/auth/login-form";
 
 interface LoginButtonProps {
     children: React.ReactNode;
@@ -17,9 +19,14 @@ export const LoginButton = ({children, mode = "redirect", asChild}: LoginButtonP
 
     if (mode === "modal") {
         return (
-            <span>
-        TODO: Implement modal
-      </span>
+            <Dialog>
+                <DialogTrigger asChild={asChild}>
+                    {children}
+                </DialogTrigger>
+                <DialogContent className="p-0 w-auto bg-transparent border-none">
+                    <LoginForm />
+                </DialogContent>
+            </Dialog>
         )
     }
 
