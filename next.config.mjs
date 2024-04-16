f0967ffe4b695238e497a76cd1f7105db2c27e7d
choosenname <vlad@luckashonok.ru>
2024-04-16 17:42:55 +0300

    fix: fix socket

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+            "utf-8-validate": "commonjs utf-8-validate",
+            bufferutil: "commonjs bufferutil"
+        });
+
+        return config;
+    },
     images: {
         domains: [
             "utfs.io"
         ]
     },
-    // Добавление опции transpilePackages для транспиляции пакета next-auth
     transpilePackages: ['next-auth'],
 };
 
